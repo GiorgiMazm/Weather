e6ba36770fb06b4883b795cdd93280c602037f3d
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2022-09-07 15:31:08 +0200

    fixed controller test

--- src/test/java/weather/WeatherControllerTest.java
+++ src/test/java/weather/WeatherControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 import weather.model.Coordinate;
 
@@ -22,10 +21,10 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 public class WeatherControllerTest {
 
   @Autowired WeatherService weatherService;
-  @Autowired CoordinateService coordinateService;
   private MockRestServiceServer mockServer;
-  @Autowired private MockMvc mockMvc;
   @Autowired private RestTemplate restTemplate;
+
+  Coordinate coordinate = new Coordinate(50f, 60f, "de", "Hamburg");
 
   @BeforeEach
   public void createMockServer() {
@@ -40,21 +39,21 @@ public class WeatherControllerTest {
   @Test
   void callsApiReturnsResult() {
     mockServer
-        .expect(requestTo("/weather"))
+        .expect(requestTo("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=50.0&lon=60.0"))
         .andRespond(
             withSuccess(
                 new ClassPathResource("/weather-response.json"), MediaType.APPLICATION_JSON));
 
-    var currentLocation = coordinateService.getCurrentLocation();
+                var currentLocation = coordinate;
+                var weather = weatherService.getWeatherFor(coordinate);
 
-    var weather = weatherService.getWeatherFor(coordinateService.getCurrentLocation());
     var createdWeather = WeatherCreator.makeWeather(currentLocation, weather);
 
-    Assertions.assertThat(createdWeather.get(1).country()).isEqualTo("Germany");
+    Assertions.assertThat(createdWeather.get(1).country()).isEqualTo("de");
     Assertions.assertThat(createdWeather.get(1).city()).isEqualTo("Hamburg");
-    Assertions.assertThat(createdWeather.get(1).humidity()).isEqualTo("38.8");
-    Assertions.assertThat(createdWeather.get(1).temperature()).isEqualTo("22.2");
-    Assertions.assertThat(createdWeather.get(1).local()).isEqualTo("2022-09-02T10:00:00");
-    Assertions.assertThat(createdWeather.get(1).utc()).isEqualTo("2022-09-02T10:00:00Z");
+    Assertions.assertThat(createdWeather.get(1).humidity()).isEqualTo("46.4");
+    Assertions.assertThat(createdWeather.get(1).temperature()).isEqualTo("20.3");
+    Assertions.assertThat(createdWeather.get(1).local()).isEqualTo("2022-09-01T12:00");
+    Assertions.assertThat(createdWeather.get(1).utc()).isEqualTo("2022-09-01T12:00Z");
   }
 }
